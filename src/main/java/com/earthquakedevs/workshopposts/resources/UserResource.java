be1b976c73be7b2c74efc29a024ea347b6e5bda0
williamdevide <williamdevide@gmail.com>
2023-11-03 15:35:09 -0300

    Endpoint Post of User

--- src/main/java/com/earthquakedevs/workshopposts/resources/UserResource.java
+++ src/main/java/com/earthquakedevs/workshopposts/resources/UserResource.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.earthquakedevs.workshopposts.domain.Post;
 import com.earthquakedevs.workshopposts.domain.User;
 import com.earthquakedevs.workshopposts.dto.UserDTO;
 import com.earthquakedevs.workshopposts.services.UserService;
@@ -54,5 +62,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
