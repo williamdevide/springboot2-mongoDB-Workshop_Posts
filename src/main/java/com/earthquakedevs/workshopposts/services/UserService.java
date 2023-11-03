1e5da89ceb887a4ea3ab3e8b73a5966172eea0c5
williamdevide <williamdevide@gmail.com>
2023-11-03 12:37:30 -0300

    Insert

--- src/main/java/com/earthquakedevs/workshopposts/services/UserService.java
+++ src/main/java/com/earthquakedevs/workshopposts/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.earthquakedevs.workshopposts.domain.User;
+import com.earthquakedevs.workshopposts.dto.UserDTO;
 import com.earthquakedevs.workshopposts.repository.UserRepository;
 import com.earthquakedevs.workshopposts.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,29 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
