eb4918da83851a6848ffab4b2d368a61aeedd45c
williamdevide <williamdevide@gmail.com>
2023-11-03 16:27:15 -0300

    Search with Query Methods with Lambda with many criterias

--- src/main/java/com/earthquakedevs/workshopposts/services/PostService.java
+++ src/main/java/com/earthquakedevs/workshopposts/services/PostService.java
@@ -1,5 +1,6 @@
 package com.earthquakedevs.workshopposts.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.earthquakedevs.workshopposts.domain.Post;
-import com.earthquakedevs.workshopposts.domain.User;
-import com.earthquakedevs.workshopposts.dto.UserDTO;
 import com.earthquakedevs.workshopposts.repository.PostRepository;
 import com.earthquakedevs.workshopposts.services.exception.ObjectNotFoundException;
 
@@ -33,5 +32,10 @@ public class PostService {
 	
 	public List<Post> searchTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
