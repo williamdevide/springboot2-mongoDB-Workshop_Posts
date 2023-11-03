192aa7675274797c80c6786bbb1a478792059f7b
williamdevide <williamdevide@gmail.com>
2023-11-03 15:31:33 -0300

    Referencing Posts

--- src/main/java/com/earthquakedevs/workshopposts/domain/User.java
+++ src/main/java/com/earthquakedevs/workshopposts/domain/User.java
@@ -1,8 +1,11 @@
 package com.earthquakedevs.workshopposts.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
@@ -16,8 +19,8 @@ public class User implements Serializable {
 	private String name;
 	private String email;
 	
-//	@DBRef(lazy = true)
-//	private List<Post> posts = new ArrayList<>();
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 	}
@@ -52,7 +55,7 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-/*
+
 	public List<Post> getPosts() {
 		return posts;
 	}
@@ -60,7 +63,7 @@ public class User implements Serializable {
 	public void setPosts(List<Post> posts) {
 		this.posts = posts;
 	}
-*/
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
