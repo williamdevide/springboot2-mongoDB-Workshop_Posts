f2f79fa988d7030e374a6dd7d215e7faa78c2fb9
williamdevide <williamdevide@gmail.com>
2023-11-03 14:57:15 -0300

    Delete and Update

--- src/main/java/com/earthquakedevs/workshopposts/resources/UserResource.java
+++ src/main/java/com/earthquakedevs/workshopposts/resources/UserResource.java
@@ -41,4 +41,18 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@DeleteMapping(value="/{id}")
+ 	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
