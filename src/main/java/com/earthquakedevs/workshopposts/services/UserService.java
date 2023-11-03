3fbdbb0852d9c9aee9f9776c017519421bfe8627
williamdevide <williamdevide@gmail.com>
2023-11-03 12:26:30 -0300

    UserById

--- src/main/java/com/earthquakedevs/workshopposts/services/UserService.java
+++ src/main/java/com/earthquakedevs/workshopposts/services/UserService.java
@@ -1,20 +1,27 @@
 package com.earthquakedevs.workshopposts.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.earthquakedevs.workshopposts.domain.User;
 import com.earthquakedevs.workshopposts.repository.UserRepository;
+import com.earthquakedevs.workshopposts.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
-	}	
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
