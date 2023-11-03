ad7d52d706a9eadcb9c9be27e55ab258168364dd
williamdevide <williamdevide@gmail.com>
2023-11-03 15:21:21 -0300

    Entity Post with User

---
+++ src/main/java/com/earthquakedevs/workshopposts/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.earthquakedevs.workshopposts.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.earthquakedevs.workshopposts.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
